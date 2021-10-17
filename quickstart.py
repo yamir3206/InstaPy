c5c8ef965d737c2c1a08a7f06c41bbb6cc679075
yamir3206 <66174799+yamir3206@users.noreply.github.com>
2021-10-16 23:12:41 -0700

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,20 +1,50 @@
-""" Quickstart script for InstaPy usage """
 
-# imports
 from instapy import InstaPy
 from instapy import smart_run
-from instapy import set_workspace
 
+insta_username = ''
+insta_password = ''
 
-# set workspace folder at desired location (default is at your home folder)
-set_workspace(path=None)
+# get a session!
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=True,
+                  multi_logs=True)
 
-# get an InstaPy session!
-session = InstaPy()
-
+# let's go! :>
 with smart_run(session):
     # general settings
-    session.set_dont_include(["friend1", "friend2", "friend3"])
+    session.set_relationship_bounds(enabled=True,
+                                    potency_ratio=None,
+                                    delimit_by_numbers=True,
+                                    max_followers=6000,
+                                    max_following=3000,
+                                    min_followers=30,
+                                    min_following=30)
+    session.set_user_interact(amount=2, randomize=True, percentage=30,
+                              media='Photo')
+    session.set_do_like(enabled=True, percentage=100)
+    session.set_do_comment(enabled=True, percentage=5)
+    session.set_comments(
+        ['Nice shot! @{}', 'I love your profile! @{}', '@{} Love it!',
+        '@{} :heart::heart:',
+        'Love your posts @{}',
+        'Looks awesome @{}',
+        'Getting inspired by you @{}',
+        ':raised_hands: Yes!',
+         '@{}:revolving_hearts::revolving_hearts:', '@{}:fire::fire::fire:'],
+        media='Photo')
 
-    # activity
-    session.like_by_tags(["natgeo"], amount=10)
+    # unfollow activity
+    session.unfollow_users(amount=126, nonFollowers=True, style="RANDOM",
+                           unfollow_after=42 * 60 * 60, sleep_delay=300)
+
+    # follow activity
+    ammount_number = 500
+    session.follow_user_followers(['chrisburkard', 'danielkordan'],
+                                  amount=ammount_number, randomize=False,
+                                  interact=True, sleep_delay=240)
+
+    """ Joining Engagement Pods...
+    """
+    session.join_pods(topic='entertainment', engagement_mode='no_comments')
